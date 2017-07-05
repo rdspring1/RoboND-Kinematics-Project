8482e4e2f2c8cfa37afadc2b0cf6081dbb9892ce
Ryan DS <rdspring1@gmail.com>
2017-07-05 09:09:42 -0700

    Forward Kinematics

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -32,25 +32,98 @@ def handle_calculate_IK(req):
             joint_trajectory_point = JointTrajectoryPoint()
 
             # Define DH param symbols
-
-
+            alpha0, alpha1, alpha2, alpha3, alpha4, alpha5, alphaG = symbols('alpha0:7')
+            a0, a1, a2, a3, a4, a5, aG = symbols('a0:7')
+            d1, d2, d3, d4, d5, d6, dG = symbols('d1:8')
+            q1, q2, q3, q4, q5, q6, qG = symbols('q1:8')
             
             # Joint angle symbols
-
-
+            theta1, theta2, theta3, theta4, theta5, theta6 = symbols('theta0:7')
       
             # Modified DH params
+            s = {alpha0:     0,  a0:   0, d1: d01,
+                 alpha1: -pi/2,  a1: a12, d2:   0, q2: q2-pi/2,
+                 alpha2:     0,  a2: a23, d3:   0,
+                 alpha3: -pi/2,  a3: a34, d4: d34,
+                 alpha4:  pi/2,  a4:   0, d5:   0,
+                 alpha5: -pi/2,  a5:   0, d6:   0,
+                 alphaG:     0,  aG:   0, dG: d5G, qG: 0}
 
+            a12 = 0.35
+            a23 = 1.25
+            a34 = -0.054
+            d01 = 0.75
+            d34 = 1.50
+            d5G = 0.303
 
             
             # Define Modified DH Transformation matrix
-
-
-
             # Create individual transformation matrices
+            T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
+                           [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+                           [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+                           [                   0,                   0,            0,               1]])
+            T0_1 = T0_1.subs(s)
 
+            T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
+                           [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+                           [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+                           [                   0,                   0,            0,               1]])
+            T1_2 = T1_2.subs(s)
 
-            
+            T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
+                           [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+                           [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+                           [                   0,                   0,            0,               1]])
+            T2_3 = T2_3.subs(s)
+
+            T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
+                           [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+                           [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+                           [                   0,                   0,            0,               1]])
+            T3_4 = T3_4.subs(s)
+
+            T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
+                           [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+                           [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+                           [                   0,                   0,            0,               1]])
+            T4_5 = T4_5.subs(s)
+
+            T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
+                           [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+                           [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+                           [                   0,                   0,            0,               1]])
+            T5_6 = T5_6.subs(s)
+
+            T6_G = Matrix([[             cos(qG),            -sin(qG),            0,              aG],
+                           [ sin(qG)*cos(alphaG), cos(q7)*cos(alphaG), -sin(alphaG), -sin(alphaG)*dG],
+                           [ sin(qG)*sin(alphaG), cos(q7)*sin(alphaG),  cos(alphaG),  cos(alphaG)*dG],
+                           [                   0,                   0,            0,               1]])
+            T6_G = T5_6.subs(s)
+
+            # Composition of Homogenous Transforms
+            T0_2 = simplify(T0_1 * T1_2)
+            T0_3 = simplify(T0_2 * T2_3)
+            T0_4 = simplify(T0_3 * T3_4)
+            T0_5 = simplify(T0_4 * T4_5)
+            T0_6 = simplify(T0_5 * T5_6)
+            T0_G = simplify(T0_6 * T6_G)
+
+            # Gripper_Link - URDF to DH Convention - Correction
+            R_z = Matrix([[     cos(np.pi),     -sin(np.pi),     0,     0],
+                          [     sin(np.pi),      cos(np.pi),     0,     0],
+                          [              0,               0,     1,     0],
+                          [              0,               0,     0,     1]])
+
+            R_y = Matrix([[     cos(-np.pi/2),               0,     sin(-np.pi/2),     0],
+                          [                 0,               1,                 0,     0],
+                          [    -sin(-np.pi/2),               0,     cos(-np.pi/2),     0],
+                          [                 0,               0,                 0,     1]])
+            R_correction = simplify(R_z * R_y)
+
+            # Total Homogenous Transformation between Base_Link and Gripper_Link with Orientation Correction
+            T_total = simplify(T0_G * R_correction)
+
             # Extract end-effector position and orientation from request
 	    # px,py,pz = end-effector position
 	    # roll, pitch, yaw = end-effector orientation
@@ -62,10 +135,7 @@ def handle_calculate_IK(req):
                 [req.poses[x].orientation.x, req.poses[x].orientation.y,
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
      
-            # Calculate joint angles using Geometric IK method
-
-		
-
+            # TODO Calculate joint angles using Geometric IK method
 
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
