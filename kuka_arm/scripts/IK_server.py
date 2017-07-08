4b7e0ee18a9e50d555d2c27fb2ca91bb06422976
Ryan DS <rdspring1@gmail.com>
2017-07-08 10:17:29 -0700

    Inverse Kinematics - Geometric - Not-Tested

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -63,42 +63,44 @@ def handle_calculate_IK(req):
                            [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
                            [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
                            [                   0,                   0,            0,               1]])
-            T0_1 = T0_1.subs(s)
 
             T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
                            [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
                            [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
                            [                   0,                   0,            0,               1]])
-            T1_2 = T1_2.subs(s)
 
             T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
                            [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
                            [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
                            [                   0,                   0,            0,               1]])
-            T2_3 = T2_3.subs(s)
 
             T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
                            [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
                            [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
                            [                   0,                   0,            0,               1]])
-            T3_4 = T3_4.subs(s)
 
             T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
                            [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
                            [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
                            [                   0,                   0,            0,               1]])
-            T4_5 = T4_5.subs(s)
 
             T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
                            [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
                            [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
                            [                   0,                   0,            0,               1]])
-            T5_6 = T5_6.subs(s)
 
             T6_G = Matrix([[             cos(qG),            -sin(qG),            0,              aG],
                            [ sin(qG)*cos(alphaG), cos(q7)*cos(alphaG), -sin(alphaG), -sin(alphaG)*dG],
                            [ sin(qG)*sin(alphaG), cos(q7)*sin(alphaG),  cos(alphaG),  cos(alphaG)*dG],
                            [                   0,                   0,            0,               1]])
+
+            # Substitute DH parameters into Transformation Matrices
+            T0_1 = T0_1.subs(s)
+            T1_2 = T1_2.subs(s)
+            T2_3 = T2_3.subs(s)
+            T3_4 = T3_4.subs(s)
+            T4_5 = T4_5.subs(s)
+            T5_6 = T5_6.subs(s)
             T6_G = T5_6.subs(s)
 
             # Composition of Homogenous Transforms
@@ -108,6 +110,15 @@ def handle_calculate_IK(req):
             T0_5 = simplify(T0_4 * T4_5)
             T0_6 = simplify(T0_5 * T5_6)
             T0_G = simplify(T0_6 * T6_G)
+
+            # Numerically evaluate transforms (compare against output from tf_echo)
+            #print("T0_1 = ", T0_1.evalf(subs={q1: 0, q2: 0, q3:0, q4:0, q5:0, q6:0}))
+            #print("T0_2 = ", T0_2.evalf(subs={q1: 0, q2: 0, q3:0, q4:0, q5:0, q6:0}))
+            #print("T0_3 = ", T0_3.evalf(subs={q1: 0, q2: 0, q3:0, q4:0, q5:0, q6:0}))
+            #print("T0_4 = ", T0_4.evalf(subs={q1: 0, q2: 0, q3:0, q4:0, q5:0, q6:0}))
+            #print("T0_5 = ", T0_5.evalf(subs={q1: 0, q2: 0, q3:0, q4:0, q5:0, q6:0}))
+            #print("T0_6 = ", T0_6.evalf(subs={q1: 0, q2: 0, q3:0, q4:0, q5:0, q6:0}))
+            #print("T0_G = ", T0_G.evalf(subs={q1: 0, q2: 0, q3:0, q4:0, q5:0, q6:0}))
 
             # Gripper_Link - URDF to DH Convention - Correction
             R_z = Matrix([[     cos(np.pi),     -sin(np.pi),     0,     0],
@@ -136,7 +147,61 @@ def handle_calculate_IK(req):
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
      
             # TODO Calculate joint angles using Geometric IK method
+            # Calculate Wrist Center
+            wx = px - d5G * T_total[2,0]
+            wy = py - d5G * T_total[2,1] 
+            wz = pz - d5G * T_total[2,2]
 
+            # Calculate Position, q1, q2, q3
+            # ELBOW - DOWN
+            height = wz - d01
+            width = sqrt(power(wx, 2.0) + power(wy, 2.0)) - a12
+            l = sqrt(power(height, 2.0) + power(width, 2.0))
+
+            alpha = (height, width)
+            beta = (power(a23, 2.0) + power(d34, 2.0) - power(l, 2.0)) / (2 * a23 * d34)
+            gamma = (power(l, 2.0) + power(a23, 2.0) + power(d34, 2.0)) / (2 * a23 * l)
+
+            theta1 = atan2(wy, wx)
+            theta2 = alpha - gamma
+            theta3 = pi - beta
+            
+            # Calculate T3_6
+            T0_3_sub = T0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3}))
+            T3_6 = Transpose(T0_3_sub) * T0_6
+
+            # Using q1, q2, q3, calculate Orientation, q4, q5, q6
+            r13 = T3_6[0,2]
+            r23 = T3_6[1,2]
+            r33 = T3_6[2,2]
+
+            if r13 == 0.0 and r23 == 0.0:
+                # Singular - Arbitrarily set theta 4 to pi/2
+                theta5 = 0
+
+                T3_6 = T3_6.subs({q5: theta5})
+                r21 = T3_6[1,0]
+                r11 = T3_6[0,0]
+                theta46 = atan2(r21, r11)
+                theta4 = pi / 2
+                theta6 = theta46 - theta4
+            else:
+                # Non-Singular
+                theta5 = atan2(sqrt(1-power(r33, 2.0)), r33) 
+
+                T3_6 = T3_6.subs({q5: theta5})
+                r13 = T3_6[0.2]
+                r23 = T3_6[1,2]
+                r32 = T3_6[2,1]
+                r31 = T3_6[2,0]
+
+                if theta5 > 0.0:
+                    theta4 = atan2( r23,  r13)
+                    theta6 = atan2( r32, -r31)
+                else:
+                    theta4 = atan2(-r23, -r13)
+                    theta6 = atan2(-r32,  r31)
+            
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
 	    joint_trajectory_point.positions = [theta1, theta2, theta3, theta4, theta5, theta6]
