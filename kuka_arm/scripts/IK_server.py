f1d5c7dc319a49ee5e58b15ee39d8ac98262ad65
Ryan DS <rdspring1@gmail.com>
2017-07-09 21:17:57 -0700

    Inverse Kinematics - q1,q2,q3 - Success

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -44,43 +44,42 @@ s = {alpha0:     0,  a0:   0, d1: d01,
      alpha5: -pi/2,  a5:   0, d6:   0,
      alphaG:     0,  aG:   0, dG: d5G, qG: 0}
 
-
 # Define Modified DH Transformation matrix
 # Create individual transformation matrices
 T0_1 = Matrix([[             cos(q1),            -sin(q1),            0,              a0],
-   [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
-   [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
-   [                   0,                   0,            0,               1]])
+               [ sin(q1)*cos(alpha0), cos(q1)*cos(alpha0), -sin(alpha0), -sin(alpha0)*d1],
+               [ sin(q1)*sin(alpha0), cos(q1)*sin(alpha0),  cos(alpha0),  cos(alpha0)*d1],
+               [                   0,                   0,            0,               1]])
 
 T1_2 = Matrix([[             cos(q2),            -sin(q2),            0,              a1],
-   [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
-   [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
-   [                   0,                   0,            0,               1]])
+               [ sin(q2)*cos(alpha1), cos(q2)*cos(alpha1), -sin(alpha1), -sin(alpha1)*d2],
+               [ sin(q2)*sin(alpha1), cos(q2)*sin(alpha1),  cos(alpha1),  cos(alpha1)*d2],
+               [                   0,                   0,            0,               1]])
 
 T2_3 = Matrix([[             cos(q3),            -sin(q3),            0,              a2],
-   [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
-   [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
-   [                   0,                   0,            0,               1]])
+               [ sin(q3)*cos(alpha2), cos(q3)*cos(alpha2), -sin(alpha2), -sin(alpha2)*d3],
+               [ sin(q3)*sin(alpha2), cos(q3)*sin(alpha2),  cos(alpha2),  cos(alpha2)*d3],
+               [                   0,                   0,            0,               1]])
 
 T3_4 = Matrix([[             cos(q4),            -sin(q4),            0,              a3],
-   [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
-   [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
-   [                   0,                   0,            0,               1]])
+               [ sin(q4)*cos(alpha3), cos(q4)*cos(alpha3), -sin(alpha3), -sin(alpha3)*d4],
+               [ sin(q4)*sin(alpha3), cos(q4)*sin(alpha3),  cos(alpha3),  cos(alpha3)*d4],
+               [                   0,                   0,            0,               1]])
 
 T4_5 = Matrix([[             cos(q5),            -sin(q5),            0,              a4],
-   [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
-   [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
-   [                   0,                   0,            0,               1]])
+               [ sin(q5)*cos(alpha4), cos(q5)*cos(alpha4), -sin(alpha4), -sin(alpha4)*d5],
+               [ sin(q5)*sin(alpha4), cos(q5)*sin(alpha4),  cos(alpha4),  cos(alpha4)*d5],
+               [                   0,                   0,            0,               1]])
 
 T5_6 = Matrix([[             cos(q6),            -sin(q6),            0,              a5],
-   [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
-   [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
-   [                   0,                   0,            0,               1]])
+               [ sin(q6)*cos(alpha5), cos(q6)*cos(alpha5), -sin(alpha5), -sin(alpha5)*d6],
+               [ sin(q6)*sin(alpha5), cos(q6)*sin(alpha5),  cos(alpha5),  cos(alpha5)*d6],
+               [                   0,                   0,            0,               1]])
 
 T6_G = Matrix([[             cos(qG),            -sin(qG),            0,              aG],
-   [ sin(qG)*cos(alphaG), cos(qG)*cos(alphaG), -sin(alphaG), -sin(alphaG)*dG],
-   [ sin(qG)*sin(alphaG), cos(qG)*sin(alphaG),  cos(alphaG),  cos(alphaG)*dG],
-   [                   0,                   0,            0,               1]])
+               [ sin(qG)*cos(alphaG), cos(qG)*cos(alphaG), -sin(alphaG), -sin(alphaG)*dG],
+               [ sin(qG)*sin(alphaG), cos(qG)*sin(alphaG),  cos(alphaG),  cos(alphaG)*dG],
+               [                   0,                   0,            0,               1]])
 
 # Substitute DH parameters into Transformation Matrices
 T0_1 = T0_1.subs(s)
@@ -89,7 +88,7 @@ T2_3 = T2_3.subs(s)
 T3_4 = T3_4.subs(s)
 T4_5 = T4_5.subs(s)
 T5_6 = T5_6.subs(s)
-T6_G = T5_6.subs(s)
+T6_G = T6_G.subs(s)
 
 # Composition of Homogenous Transforms
 T0_2 = simplify(T0_1 * T1_2)
@@ -110,37 +109,41 @@ T0_G = simplify(T0_6 * T6_G)
 
 # Gripper_Link - URDF to DH Convention - Correction
 R_z = Matrix([[     cos(pi),     -sin(pi),              0,     0],
-  [     sin(pi),      cos(pi),              0,     0],
-  [           0,            0,              1,     0],
-  [           0,            0,              0,     1]])
+              [     sin(pi),      cos(pi),              0,     0],
+              [           0,            0,              1,     0],
+              [           0,            0,              0,     1]])
 
 R_y = Matrix([[     cos(-pi/2),         0,     sin(-pi/2),     0],
-  [              0,         1,              0,     0],
-  [    -sin(-pi/2),         0,     cos(-pi/2),     0],
-  [              0,         0,              0,     1]])
+              [              0,         1,              0,     0],
+              [    -sin(-pi/2),         0,     cos(-pi/2),     0],
+              [              0,         0,              0,     1]])
 R_correction = simplify(R_z * R_y)
 
 # Total Homogenous Transformation between Base_Link and Gripper_Link with Orientation Correction
 T_total = simplify(T0_G * R_correction)
 
 # Calculate R_RPY using roll, pitch, yaw
+'''
 sym_alpha, sym_beta, sym_gamma = symbols('sym0:3')
-R_x = Matrix([[ cos(sym_alpha), -sin(sym_alpha),        0],
-  [ sin(sym_alpha),  cos(sym_alpha),        0],
-  [             0,                0,        1]])
+
+R_x = Matrix([[ 1,                     0,               0],
+              [ 0,        cos(sym_alpha), -sin(sym_alpha)],
+              [ 0,        sin(sym_alpha),  cos(sym_alpha)]])
 
 R_y = Matrix([[ cos(sym_beta),         0,  sin(sym_beta)],
-  [              0,        1,               0],
-  [-sin(sym_beta),         0,  cos(sym_beta)]])
+              [              0,        1,               0],
+              [-sin(sym_beta),         0,  cos(sym_beta)]])
 
-R_z = Matrix([[ 1,                     0,               0],
-  [ 0,        cos(sym_gamma), -sin(sym_gamma)],
-  [ 0,        sin(sym_gamma),  cos(sym_gamma)]])
+R_z = Matrix([[ cos(sym_gamma), -sin(sym_gamma),        0],
+              [ sin(sym_gamma),  cos(sym_gamma),        0],
+              [             0,                0,        1]])
 
 R_RPY = simplify(R_z * R_y * R_x)
 empty = Matrix([[0.], [0.], [0.]])
 base = Matrix([[ 0., 0., 0., 1.]])
 T_RPY = R_RPY.row_join(empty).col_join(base)
+T_RPY_sub = T_RPY.evalf(subs={sym_alpha : roll, sym_beta : pitch, sym_gamma : yaw}) * R_correction
+'''
 
 def handle_calculate_IK(req):
     rospy.loginfo("Received %s eef-poses from the plan" % len(req.poses))
@@ -167,11 +170,15 @@ def handle_calculate_IK(req):
                     req.poses[x].orientation.z, req.poses[x].orientation.w])
      
             # TODO Calculate joint angles using Geometric IK method
-            T_RPY_sub = T_RPY.evalf(subs={sym_alpha : roll, sym_beta : pitch, sym_gamma : yaw})
+            R_RPY = Matrix(tf.transformations.euler_matrix(roll,pitch,yaw, axes='sxyz'))
+            R_RPY[0,3] = px 
+            R_RPY[1,3] = py 
+            R_RPY[2,3] = pz 
+            T_RPY_sub = R_RPY * R_correction
 
             # Calculate Wrist Center
-            wx = px - d5G * T_RPY_sub[2,0]
-            wy = py - d5G * T_RPY_sub[2,1] 
+            wx = px - d5G * T_RPY_sub[0,2]
+            wy = py - d5G * T_RPY_sub[1,2] 
             wz = pz - d5G * T_RPY_sub[2,2]
 
             # Calculate Position, q1, q2, q3
@@ -181,13 +188,19 @@ def handle_calculate_IK(req):
             l = sqrt(math.pow(height, 2.0) + math.pow(width, 2.0))
 
             alpha = atan2(height, width)
-            beta = (math.pow(a23, 2.0) + math.pow(d34, 2.0) - math.pow(l, 2.0)) / (2 * a23 * d34)
-            gamma = (math.pow(l, 2.0) + math.pow(a23, 2.0) + math.pow(d34, 2.0)) / (2 * a23 * l)
+            beta = (math.pow(a23, 2.0) + math.pow(d34, 2.0) - math.pow(l, 2.0)) / (2.0 * a23 * d34)
+            gamma = (math.pow(l, 2.0) + math.pow(a23, 2.0) - math.pow(d34, 2.0)) / (2.0 * a23 * l)
 
-            theta1 = atan2(wy, wx)
-            theta2 = alpha - gamma
-            theta3 = pi - beta
-            
+            theta3_correction = 0.03459
+            sym_theta1 = atan2(wy, wx)
+            sym_theta2 = pi/2 - (alpha + atan2(sqrt(1 - math.pow(gamma, 2.0)), gamma))
+            sym_theta3 = pi/2 - theta3_correction - atan2(sqrt(1 - math.pow(beta, 2.0)), beta)
+
+            theta1 = sym_theta1.evalf()
+            theta2 = sym_theta2.evalf()
+            theta3 = sym_theta3.evalf()
+
+            '''
             # Calculate T3_6
             T0_3_sub = T0_3.evalf(subs={q1: theta1, q2: theta2, q3: theta3})
             T3_6 = Transpose(T0_3_sub) * T_RPY_sub
@@ -225,6 +238,7 @@ def handle_calculate_IK(req):
                 else:
                     theta4 = atan2(-r23, -r13)
                     theta6 = atan2(-r32,  r31)
+            '''
             
             # Populate response for the IK request
             # In the next line replace theta1,theta2...,theta6 by your joint angle variables
